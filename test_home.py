a2ae31908aff18e467b2cd6459329c1619e782a3
楹傑 邱 <pupst91168@hotmail.com>
2019-06-15 22:45:15 +0800

    為了讓CRASH的部分可以FAIL

--- test_home.py
+++ test_home.py
@@ -56,4 +56,4 @@ class HomeTest(piggy_test_case.PiggyTestCase):
         self.myDevice.submit()
         self.wait(5)
         # self.assertEqual(self.myDevice.current_activity(),".Launcher") #.Launcher 代表app crash回到主畫面
-        self.assertTrue(self.myDevice.crash())
+        self.assertFalse(self.myDevice.crash())
